3e9192cff99423043813cb49f37dbbf5a1988929
Eddie Grinman <egrinman@c02fv0m1md6v.corp.local>
2021-08-08 12:22:52 -0400

    fixed the formatting

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -4,7 +4,7 @@ def create_database(database_path: str):
     # TODO: generate the database
     pass
 
+
 def save_words_to_database(database_path: str, word_list: list):
     # TODO: save words to database
     pass
-
